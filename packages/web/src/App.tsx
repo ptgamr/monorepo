1d43c287eb7f315dbe6a5fdbf890fff1847491f4
Anh <anh.trinhtrung@gmail.com>
2021-06-26 22:53:24 +1200

    Adding blueprint to web

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,9 +1,11 @@
+import { Button, Classes } from "@blueprintjs/core";
 import * as React from "react";
 
 export function App() {
   return (
-    <div>
+    <div className={Classes.DARK}>
       <h1>Hello from Monorepo</h1>
+      <Button>Click Me</Button>
     </div>
   );
 }
