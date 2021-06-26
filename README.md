30ae93c8334b99d4b7dcb4cfc440ecf99c8ee4ec
Anh <anh.trinhtrung@gmail.com>
2021-06-26 23:30:55 +1200

    button can be render but styles broken

--- README.md
+++ README.md
@@ -4,4 +4,11 @@
 
 https://github.com/NiGhTTraX/ts-monorepo
 
+https://www.baltuta.eu/posts/typescript-lerna-monorepo-the-setup
+
+
+Why adding something to the root package.json bad?
+
+https://www.reddit.com/r/javascript/comments/9t6yht/yarn_workspaces_why_is_adding_something_to_the/
+
 
--- packages/app/src/components/Application.tsx
+++ packages/app/src/components/Application.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
-import { hot } from 'react-hot-loader';
-import logo from '@assets/images/logo.png';
-import './Application.less';
+import React, { useState } from "react";
+import { hot } from "react-hot-loader";
+import logo from "@assets/images/logo.png";
+import { WebButton } from "@monorepo/web/src/WebButton";
+import "./Application.less";
 
 type Props = {
   title: string;
@@ -14,32 +15,32 @@ const Application: React.FC<Props> = (props) => {
   return (
     <React.Fragment>
       <main>
-        <div className='main-heading'>
-          <img src={logo} width='32' title='Codesbiome' />
+        <div className="main-heading">
+          <img src={logo} width="32" title="Codesbiome" />
           <h1>{props.title}</h1>
         </div>
-        <p className='main-teaser'>
+        <p className="main-teaser">
           Custom boilerplate for rapid development of Desktop Applications.
           <br />
           This project makes use of Electron, React, Typescript, Webpack to
           serve the best environment for development with hot-reloading of
           modules.
         </p>
-        <div className='versions'>
+        <div className="versions">
           <span>
             ERWT <span>{props.version}</span>
           </span>
           <span>
-            Electron <span id='electron-version'></span>
+            Electron <span id="electron-version"></span>
           </span>
           <span>
-            Chrome <span id='chrome-version'></span>
+            Chrome <span id="chrome-version"></span>
           </span>
           <span>
-            Node <span id='node-version'></span>
+            Node <span id="node-version"></span>
           </span>
         </div>
-        <p className='main-teaser small'>
+        <p className="main-teaser small">
           Click below button to update the application &quot;counter&quot;
           state. Components will update their states using
           Hot-Module-Replacement technique, without needing to refresh/reload
@@ -48,6 +49,7 @@ const Application: React.FC<Props> = (props) => {
         <button onClick={() => setCounter(counter + 1)}>
           Counter <span>{counter}</span>
         </button>
+        <WebButton />
       </main>
     </React.Fragment>
   );
